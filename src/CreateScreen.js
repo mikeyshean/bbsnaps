d4cfb08d3d0a89d3c9e5c02e47e6053c213d1fc0
Mikey Shean <mdshean2@gmail.com>
2016-10-03 20:58:28 -0400

    Add camera module.

--- src/CreateScreen.js
+++ src/CreateScreen.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { Component } from 'react';
+import { Text, Dimensions } from 'react-native';
+import Camera from 'react-native-camera';
 import { ViewContainer } from './components/common';
 // import { Actions } from 'react-native-router-flux';
 
-const CreateScreen = () => {
-  return (
-    <ViewContainer>
-      <Text
-        style={styles.textStyle}
-      >
-        Create!
-      </Text>
-    </ViewContainer>
-  );
-};
+class CreateScreen extends Component {
+
+  takePicture() {
+    this.camera.capture()
+    .then((data) => console.log(data))
+    .catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <ViewContainer>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}
+        >
+          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+        </Camera>
+      </ViewContainer>
+    );
+  }
+}
 
 const styles = {
   textStyle: {
@@ -21,6 +35,21 @@ const styles = {
     textAlign: 'center',
     margin: 10,
     color: '#ffffff'
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
   }
 };
 
